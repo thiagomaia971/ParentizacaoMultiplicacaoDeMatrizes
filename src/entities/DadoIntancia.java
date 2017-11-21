7f6572d45a5a0baf5bf6692d4a78f2120a9422ec
Thiago Maia <thiago.maia971@gmail.com>
2017-11-21 13:26:47 -0300

    Fazendo recursão no alg. guloso

--- src/entities/DadoIntancia.java
+++ src/entities/DadoIntancia.java
@@ -1,9 +1,13 @@
 package entities;
 
 public class DadoIntancia {
+    private int quantidadeDeMatrizes;
     private int[] values;
+
     public DadoIntancia(String linha) {
         String[] a = linha.split(" ");
+        quantidadeDeMatrizes = Integer.parseInt(a[0]);
+
         values = new int[a.length - 1];
         for (int i = 1; i < a.length; i++) {
             values[i - 1] = Integer.parseInt(a[i]);
@@ -12,5 +16,9 @@ public class DadoIntancia {
 
     public int[] getValues() {
         return values;
+    }
+
+    public int getQuantidadeDeMatrizes() {
+        return quantidadeDeMatrizes;
     }
 }
--- src/services/parentizadores/ParentizadorPorAlgoritmoGuloso.java
+++ src/services/parentizadores/ParentizadorPorAlgoritmoGuloso.java
@@ -30,16 +30,52 @@ public class ParentizadorPorAlgoritmoGuloso implements IParentizador {
     }
 
     private int c(int[] p, int i, int j) {
-        if (i + 2 == j)
-            return p[i] * p[i + 1] * p[j];
+        int valor = 0;
 
-        Log.print("(");
+        if (i == j || j - i == 1) {
+            Log.print("A" + i);
+        } else {
+            Log.print("(");
 
-        int left = p[i] * p[i + 1] * p[j];
-        int rigth = c(p, i + 1, j);
+            int m = (j - i) / 2;
+            int left = c(p, i, m);
+            int rigth = c(p, m, j);
+            valor = p[i] * p[m] * p[j];
 
-        Log.print(")");
-        return left;
+            Log.print(")");
+        }
+
+        return valor;
+
+//        if (matrizCurrentIndex + 1 == quantidadeDeMatriazes) {
+//            valor += p[matrizCurrentIndex - 1] * p[matrizCurrentIndex] * p[matrizCurrentIndex + 1];
+//            Log.print("A" + (matrizCurrentIndex) + "A" + (matrizCurrentIndex + 1));
+//        } else if (matrizCurrentIndex + 1 > quantidadeDeMatriazes){
+//            valor += p[matrizCurrentIndex - 2] * p[matrizCurrentIndex - 1] * p[matrizCurrentIndex];
+//            Log.print("A" + (matrizCurrentIndex -1) + "A" + (matrizCurrentIndex));
+//        }
+//        else {
+//            int left = p[matrizCurrentIndex - 1] * p[matrizCurrentIndex] * p[matrizCurrentIndex + 1];
+//            int rigth = p[matrizCurrentIndex] * p[matrizCurrentIndex + 1] * p[matrizCurrentIndex + 2];
+//            if (left < rigth) {
+//                valor += left;
+//                Log.print("A" + (matrizCurrentIndex ) + "A" + (matrizCurrentIndex + 1));
+//                c(p, quantidadeDeMatriazes, matrizCurrentIndex + 1);
+//            }else {
+//                valor += p[matrizCurrentIndex];
+//                Log.print("A" + matrizCurrentIndex);
+////                if (matrizCurrentIndex + 1 >= quantidadeDeMatriazes - 1) {
+////                    valor += p[matrizCurrentIndex + 1] * p[matrizCurrentIndex + ] * p[matrizCurrentIndex + 1];
+////                    Log.print("A" + (matrizCurrentIndex - 1) + "A" + (matrizCurrentIndex));
+////                }else
+//                    c(p, quantidadeDeMatriazes, matrizCurrentIndex + 1);
+//            }
+//
+//        }
+//        Log.print(")");
+//
+//        return valor;
+
 
 //        int m = (i + j) / 2;
 //        if (m == 1) {
