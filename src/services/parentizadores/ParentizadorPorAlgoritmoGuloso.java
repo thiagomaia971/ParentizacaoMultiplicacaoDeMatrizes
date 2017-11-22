c1a745a620b6787c816d7a501bdff64ff38d8a80
Thiago Maia <thiago.maia971@gmail.com>
2017-11-22 13:56:49 -0300

    Parentização por AlgoritmoGuloso

--- src/services/parentizadores/ParentizadorPorAlgoritmoGuloso.java
+++ src/services/parentizadores/ParentizadorPorAlgoritmoGuloso.java
@@ -2,24 +2,18 @@ package services.parentizadores;
 
 import entities.ArquivoEntrada;
 import entities.DadoIntancia;
-import entities.Matriz;
-import entities.Parentizacao;
 import interfaces.IParentizador;
 import services.Log;
-
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ParentizadorPorAlgoritmoGuloso implements IParentizador {
     @Override
     public void processarArquivo(ArquivoEntrada arquivosDeEntrada) {
-        Log.println("\n##Parentização de Matrizes por Algoritmo Guloso: ");
+        Log.println("\n##Parentização de Matrizes por Algoritmo Guloso!!");
 
         for (DadoIntancia instancia : arquivosDeEntrada.getInstancias()) {
             int[] p = instancia.getValues();
-
-            Log.println(" " + c(p, 1, p.length - 1));
+            int resultado = algoritmoGuloso(p, 1, p.length - 1);
+            Log.println(" " + resultado);
         }
     }
 
@@ -30,12 +24,14 @@ public class ParentizadorPorAlgoritmoGuloso implements IParentizador {
      * Conquista: Somatório da divisão
      * Tudo antes da divisão => Teta(n)(?)
      *
+     * Caso
+     *
      * @param p
      * @param i
      * @param j
      * @return
      */
-    private int c(int[] p, int i, int j) {
+    private int algoritmoGuloso(int[] p, int i, int j) {
         if (i == j) {
             Log.print("A" + i);
             return 0;
@@ -52,8 +48,8 @@ public class ParentizadorPorAlgoritmoGuloso implements IParentizador {
         }
 
         Log.print("(");
-        valor += c(p, i, auxK);
-        valor += c(p, auxK + 1, j);
+        valor += algoritmoGuloso(p, i, auxK);
+        valor += algoritmoGuloso(p, auxK + 1, j);
         Log.print(")");
 
         return valor;
--- src/services/parentizadores/ParentizadorPorProgramacaoDinamica.java
+++ src/services/parentizadores/ParentizadorPorProgramacaoDinamica.java
@@ -8,40 +8,47 @@ import services.Log;
 public class ParentizadorPorProgramacaoDinamica implements IParentizador {
     @Override
     public void processarArquivo(ArquivoEntrada arquivoDeEntrada) {
-        Log.println("Iniciando algoritmo por Programação Dinâmica!");
+        Log.println("\n##Parentização de Matrizes por Programação Dinâmica!!");
         for (DadoIntancia instancia : arquivoDeEntrada.getInstancias()) {
-            int[] p = instancia.getValues();
-            int n = p.length;
-
-            int[][] m = new int[n][n];
-            int[][] s = new int[n][n];
-
-            for (int l = 1; l < n; l++) {
-                for (int i = 0; i < n - l; i++) {
-                    int j = i + l;
-                    m[i][j] = Integer.MAX_VALUE;
-                    for (int k = i; k < j; k++) {
-                        int q = m[i][k] + m[k + 1][j] + (p[i] * p[k] * p[j]);
-                        if (q < m[i][j]) {
-                            m[i][j] = q;
-                            s[i][j] = k;
-                        }
-                    }
-                }
-            }
-
-            printar(s, 0, p.length - 1);
+            programacaoDinamica(instancia);
         }
     }
 
-    public void printar(int[][] s, int i, int j) {
+    /***
+     *
+     * @param instancia
+     */
+    private void programacaoDinamica(DadoIntancia instancia) {
+        int[] p = instancia.getValues();
+        int[][] m = new int[p.length][p.length];
+        int[][] s = new int[p.length][p.length];
+
+        for (int l = 2; l < p.length; l++) {
+            for (int i = 1; i <= p.length - l; i++) {
+                int j = i + l - 1;
+                m[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k < j; k++) {
+                    int q = m[i][k] + m[k + 1][j] + (p[i - 1] * p[k] * p[j]);
+                    if (q < m[i][j]) {
+                        m[i][j] = q;
+                        s[i][j] = k;
+                    }
+                }
+            }
+        }
+
+        printar(s, 1, p.length - 1);
+        System.out.println(" " + m[1][p.length - 1]);
+    }
+
+    private void printar(int[][] s, int i, int j) {
         if (i == j)
-            Log.print("A" + i);
+            System.out.print("A" + i);
         else {
-            Log.print("(");
+            System.out.print("(");
             printar(s, i, s[i][j]);
             printar(s, s[i][j] + 1, j);
-            Log.print(")");
+            System.out.print(")");
         }
     }
 
